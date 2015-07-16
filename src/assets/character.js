15dcada5a54d636ddcbbaf88ad826cfed7b210ef
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2015-07-16 12:22:01 -0400

    CharacterLoader now honors multistate options

--- src/assets/character.js
+++ src/assets/character.js
@@ -19,7 +19,22 @@ export default class CharacterLoader extends AssetLoader {
 
     // Add the root path to the character file
     chara.__root = Path.dirname(url);
-    return new MsSprite(chara);
+    let sprite = new MsSprite(chara);
+
+    // Apply transform options (if any)
+    if (options) {
+      let transforms = _.chain(options)
+        .mapValues(Number)
+        .value();
+      sprite.transform(transforms, { immediate: true, duration: 0 });
+    }
+
+    // Apply initial state
+    if (options && options.state) {
+      sprite.state(options.state);
+    }
+
+    return sprite;
   }
 
 }
